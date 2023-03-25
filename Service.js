82028471e2a3221e0c9b4591b5f1a7f05b66eb17
alocraxzz <alocraxzz@gmail.com>
2023-03-25 20:20:13 +0200

    update | stable

---
+++ Service.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const https = require('https');
+
+
+module.exports = {
+    baseUrl,
+    mkdirIfNotExistsSync,
+};
--- main.js
+++ main.js
@@ -1,15 +1,35 @@
 const { app, BrowserWindow, Menu } = require('electron');
-const axios = require('axios');
-const cheerio = require('cheerio');
-const fs = require('fs');
+const axios = require("axios");
+const cheerio = require("cheerio");
+const https = require("https");
+const fs = require("fs");
+
+const baseUrl = `https://russianwarship.rip`;
+
+const mkdirIfNotExistsSync = (path) => {
+    try {
+        fs.statSync(path);
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            fs.mkdirSync(path);
+        } else {
+            throw error;
+        }
+    }
+};
+
+let win;
 
 function createWindow () {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
+    win = new BrowserWindow({
+        width: 1280,
+        height: 720,
         webPreferences: {
             nodeIntegration: true,
-            contentSecurityPolicy: "default-src 'self'; script-src 'self' 'unsafe-inline';"
+            contextIsolation: false,
+            enableRemoteModule: true,
+            webSecurity: false,
+            allowRunningInsecureContent: true,
         }
     });
 
@@ -17,35 +37,133 @@ function createWindow () {
 
     Menu.setApplicationMenu(null);
 
-    axios.get('https://russianwarship.rip/en')
+    axios.get(`${baseUrl}`)
         .then(response => {
             const html = response.data;
             const $ = cheerio.load(html);
 
-            // Загружаем стили страницы через HTTP-запрос
-            axios.get('https://russianwarship.rip/dist/app.d5547f60550a7abd7256.css')
-                .then(response => {
-                    $('head').append(`<style>${response.data}</style>`);
-                    // сохраняем HTML-код страницы
-                    fs.writeFile('index.html', $.html(), err => {
-                        if (err) throw err;
-                        console.log('HTML saved!');
+            mkdirIfNotExistsSync('./styles');
+
+            $('link[rel="stylesheet"]').each(function() {
+                const href = $(this).attr('href');
+                const fileName = href.split('/').pop();
+
+                https.get(`${baseUrl}${href}`, (res) => {
+                    const filePath = `./styles/${fileName}`;
+                    const fileStream = fs.createWriteStream(filePath);
+                    res.pipe(fileStream);
+
+                    fileStream.on('finish', () => {
+                        fileStream.close();
+                        console.log(`Saved file ${fileName}`);
+                        $(this).attr('href', filePath);
+                        fs.writeFile('index.html', $.html(), err => {
+                            if (err) throw err;
+                            console.log('Styles saved!');
+                        });
                     });
-                    win.loadFile('index.html');
-                })
-                .catch(error => {
-                    console.log(error);
                 });
+            });
+
+            mkdirIfNotExistsSync('./images');
+
+            $('img').each(function() {
+                const src = $(this).attr('src');
+                const fileName = src.split('/').pop();
+
+                https.get(`${src}`, (res) => {
+                    const filePath = `./images/${fileName}`;
+                    const fileStream = fs.createWriteStream(filePath);
+                    res.pipe(fileStream);
+
+                    $(this).attr('src', filePath);
+
+                    fileStream.on('finish', () => {
+                        fileStream.close();
+                        console.log(`Saved file ${fileName}`);
+                    });
+                });
+            });
+
+            const urls = [
+                {url: `${baseUrl}/images/bg.png`, filename: `bg.png`},
+                {url: `${baseUrl}/images/bg-yellow.svg`, filename: `bg-yellow.svg`},
+                {url: `${baseUrl}/images/bg-blue.svg`, filename: `bg-blue.svg`},
+                {url: `${baseUrl}/images/warship.svg`, filename: `warship.svg`},
+                {url: `${baseUrl}/images/tooltip-bg.svg`, filename: `tooltip-bg.svg`},
+            ];
+
+            urls.forEach((elem, index) => {
+                https.get(elem.url, (res) => {
+                    const filePath = `./images/${elem.filename}`;
+                    const fileStream = fs.createWriteStream(filePath);
+                    res.pipe(fileStream);
+
+                    fileStream.on('finish', () => {
+                        fileStream.close();
+                        console.log(`Saved file ${elem.filename}`);
+                    });
+                });
+            });
+
+            mkdirIfNotExistsSync('./scripts');
+
+            $('script[src]').each(function() {
+                const src = $(this).attr('src');
+                const fileName = src.split('/').pop();
+
+                https.get(`${baseUrl}${src}`, (res) => {
+                    const filePath = `./scripts/${fileName}`;
+                    const fileStream = fs.createWriteStream(filePath);
+                    res.pipe(fileStream);
+
+                    $(this).attr('src', filePath);
+                    
+                    fileStream.on('finish', () => {
+                        fileStream.close();
+                        console.log(`Saved file ${fileName}`);
+                    });
+                });
+            });
+
+            mkdirIfNotExistsSync('./fonts');
+
+            $('link[rel="preload"]').each(function() {
+                const href = $(this).attr('href');
+                const fileName = href.split('/').pop();
+
+                https.get(`${href}`, (res) => {
+                    const filePath = `./fonts/${fileName}`;
+                    const fileStream = fs.createWriteStream(filePath);
+                    res.pipe(fileStream);
+                    
+                    $(this).attr('href', filePath);
+
+                    fileStream.on('finish', () => {
+                        fileStream.close();
+                        console.log(`Saved file ${fileName}`);
+                    });
+                });
+            });
+
+            fs.writeFile('index.html', $.html(), err => {
+                if (err) throw err;
+                console.log('HTML saved!');
+            });
         })
         .catch(error => {
             console.log(error);
         });
 
-
+    setTimeout(() => {
+        win.loadFile('index.html');
+    }, 1500);
 }
 
 app.whenReady().then(() => {
     createWindow();
+
+
 
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) {
